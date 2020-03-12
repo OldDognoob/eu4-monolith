7f869822edd1d0b138051c2867e7013a79c98a27
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-12 12:45:48 +0200

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
@@ -6,7 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 9000
+
+console.log(process.env.LADY);
 
 app.get('/api', (req, res) => {
   res.json({ message: 'The api is UP' })
